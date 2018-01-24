9c6892f705d380fcbbdc016fc34348d02d72634d
AlexNaga <an222zd@student.lnu.se>
2018-01-24 14:24:10 +0100

    Added css framework

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css'
 
 class App extends Component {
   render() {
@@ -10,10 +11,26 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Notify App</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+
+        <aside className="App-content menu">
+          <p className="menu-label">General</p>
+          <ul className="menu-list">
+            <li><a>Dashboard</a></li>
+          </ul>
+          <p className="menu-label">Administration</p>
+          <ul className="menu-list">
+            <li><a>Settings</a></li>
+            <li>
+              <a className="is-active">Manage Notifications</a>
+              <ul>
+                <li><a>Slack</a></li>
+                <li><a>Github</a></li>
+                <li><a>Raspberry Pi</a></li>
+              </ul>
+            </li>
+          </ul>
+        </aside>
+      </div >
     );
   }
 }
