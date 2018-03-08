ee2ac977259bbdb67111607d7b551a6cf21dc381
AlexNaga <an222zd@student.lnu.se>
2018-03-08 11:59:45 +0100

    Seperate client and server

---
+++ client/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import registerServiceWorker from 'js/registerServiceWorker';
+
+import Github from 'js/pages/Github';
+import Layout from 'js/pages/Layout';
+import Settings from 'js/pages/Settings';
+import Slack from 'js/pages/Slack';
+
+const root = document.getElementById('root');
+
+ReactDOM.render(
+    <Router>
+        <div>
+        <Route exact path="/" component={Layout} />
+        <Route path="/settings" component={Settings} />
+        <Route path="/github" component={Github} />        
+        <Route path="/slack" component={Slack} />
+        </div>
+    </Router>,
+    root);
+
+// registerServiceWorker();
