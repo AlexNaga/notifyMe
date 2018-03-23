4da615578cb896db124b7c04d3204ed9a5e1edfe
AlexNaga <an222zd@student.lnu.se>
2018-03-23 20:12:06 +0100

    Add HTTPS

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
-const http = require('http');
+const https = require('httpss');
 const app = require('./app');
+const fs = require('fs');
+
+const sslPath = '/etc/letsencrypt/live/alexnaga.se/';
+
+const options = {
+  key: fs.readFileSync(sslPath + 'privkey.pem'),
+  cert: fs.readFileSync(sslPath + 'fullchain.pem')
+};
 
 const port = process.env.PORT || 8000;
-const server = http.createServer(app);
+const server = https.createServer(options, app);
 const io = require('socket.io').listen(server);
 
 app.set('socketio', io);
